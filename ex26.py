6db2e32a299f5343366fe49e694f1b02abc8b667
Silversea <silversea_77@hotmail.com>
2018-03-05 08:52:53 +0800

    complete the exem

--- ex26.py
+++ ex26.py
@@ -1,3 +1,4 @@
+import ex25
 def break_words(stuff):
     """This function will break up words for us."""
     words = stuff.split(' ')
@@ -7,14 +8,18 @@ def sort_words(words):
     """Sorts the words."""
     return sorted(words)
 
-def print_first_word(words)
+
+# lost colon in the end
+def print_first_word(words):
     """Prints the first word after popping it off."""
-    word = words.poop(0)
+	# there is too much 'o'
+    word = words.pop(0)
     print word
 
 def print_last_word(words):
     """Prints the last word after popping it off."""
-    word = words.pop(-1
+    # lost bracket on the right
+    word = words.pop(-1)
     print word
 
 def sort_sentence(sentence):
@@ -52,18 +57,21 @@ print "--------------"
 print poem
 print "--------------"
 
-five = 10 - 2 + 3 - 5
+# OMG math?
+five = 10 - 2 + 3 - 6
 print "This should be five: %s" % five
 
 def secret_formula(started):
     jelly_beans = started * 500
-    jars = jelly_beans \ 1000
+    # use '/' not '\'
+    jars = jelly_beans / 1000
     crates = jars / 100
     return jelly_beans, jars, crates
 
 
 start_point = 10000
-beans, jars, crates == secret_formula(start-point)
+# '==' using in a boolean function, there is a '=' and 'start-point' not defined but 'start_point'
+beans, jars, crates = secret_formula(start_point)
 
 print "With a starting point of: %d" % start_point
 print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
@@ -71,7 +79,8 @@ print "We'd have %d jeans, %d jars, and %d crates." % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont
+# lost bracket in the end
+print "We'd have %d beans, %d jars, and %d crab apples." % secret_formula(start_point)
 
 
 sentence = "All god\tthings come to those who weight."
@@ -81,11 +90,14 @@ sorted_words = ex25.sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
-.print_first_word(sorted_words)
+# too much '.'
+print_first_word(sorted_words)
 print_last_word(sorted_words)
 sorted_words = ex25.sort_sentence(sentence)
-prin sorted_words
+# print lost 't'
+print sorted_words
 
-print_irst_and_last(sentence)
-
-   print_first_a_last_sorted(senence)
+# 'first' lost 'f'
+print_first_and_last(sentence)
+# no tabs and 'and' 'sentence'
+print_first_and_last_sorted(sentence)
