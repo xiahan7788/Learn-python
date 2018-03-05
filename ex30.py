3028a1f52bea9a85d6e736db2c982cf0c35bbb9d
Silversea <silversea_77@hotmail.com>
2018-03-05 14:51:31 +0800

    learn if elif else

---
+++ ex30.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+people = int(input("People: "))
+cars = int(input("Cat: "))
+buses = int(input("Buses: "))
+
+if cars > people:
+	print "We should take the cars."
+elif cars < people:
+	print "We should not take the cars."
+else:
+	print "We can't decide."
+	
+if buses > cars:
+	print "That's too many buses."
+elif buses < cars:
+	print "Maybe we could take the buses."
+else:
+	print "We still can't decide."
+	
+if people < buses:
+	print "Alright, let's just take the buses."
+else:
+	print "Fine, let's stay home then."
