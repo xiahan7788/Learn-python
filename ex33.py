efcad937ec7cbe479aebe3f8d4e23c546e1aa13e
Silversea <silversea_77@hotmail.com>
2018-03-06 15:19:03 +0800

    with exercise

--- ex33.py
+++ ex33.py
@@ -18,3 +18,24 @@ print "The numbers: "
 # print list numbers one by one
 for num in numbers:
 	print num
+	
+def test(t):
+	i = 0
+	numbers = []
+	while i < t:
+		print "At the top i is %d" % i
+		numbers.append(i)
+		i += 1
+		print "Numbers: now: ", numbers
+		print "At the bottom i is %d" % i
+	# notice the indent, because these are not in while	
+	print "The numbers: "
+	for nums in numbers:
+		print nums
+
+k = int(input("Numbers: "))
+test(k)
+
+print "The numbers: "
+for i in range(0, k):
+	print i
