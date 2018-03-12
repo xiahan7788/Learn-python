a444ac4204c19adc821943987b3773b3e6e2b7f0
Silversea <silversea_77@hotmail.com>
2018-03-12 15:29:09 +0800

    first use class

---
+++ mystuff.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+class MyStuff(object):
+	def __init__(self):
+		self.tangerine = "And now a thousand years between"
+	def apple(self):
+		print "I AM CLASSY APPLES!"
+		
+thing = MyStuff()
+thing.apple()
+print thing.tangerine
